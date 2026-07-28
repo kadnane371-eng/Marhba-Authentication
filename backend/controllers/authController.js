1c4af5d469f356955f5a8813b0b04cb4eb961b9d
khadija <kadnane371@gmail.com>
2026-07-28 17:19:37 +0200

    Implement user authentication with registration and login functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import  { User } from "../models/index.js";
 
 export const register = async (req, res) => {
@@ -12,17 +14,32 @@ export const register = async (req, res) => {
             return res.status(400).json({
                 message: "Email déja utilisé",
             });
-        }
+        } 
+
+       
+        
+        const hashedPassword = await bcrypt.hash(password, 10)
 
         const user = await User.create({
             fullName,
             email,
-            password,
+            password: hashedPassword,
         });
-
+         const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        );
         res.status(201).json({
             message: "Utilisateur créé avec succès",
-            user,
+            token,
+            user: {
+                id: user.id,
+                fullName: user.fullName,
+                email: user.email,
+            },
         });
     }catch (error) {
         res.status(500).json({
@@ -30,3 +47,51 @@ export const register = async (req, res) => {
         });
     }
 };
+
+ export const login = async (req, res) => {
+            try {
+                const { email, password } = req.body;
+                const user = await User.findOne({
+                    where: { email },
+                });
+
+                if (!user) {
+                    return res.status(401).json({
+                        message: "Email ou mot de passe incorrect",
+                    });
+                }
+
+
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+
+                if (!isPasswordValid) {
+                    return res.status(401).json({
+                        message: "Email ou mot de passe incorrect",
+                    });
+                }
+
+
+                const token = jwt.sign(
+                    { id: user.id },
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: "7d",
+                    }
+                );
+
+                res.status(200).json({
+                    message: "Connexion réussie",
+                    token,
+                    user: {
+                        id: user.id,
+                        fullName: user.fullName,
+                        email: user.email,
+                    },
+                });
+            }catch (error) {
+                res.status(500).json({
+                    message: error.message,
+                });
+            }
+        };
+
